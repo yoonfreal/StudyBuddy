ff24e8b8894cde3f0cf98c481f5daea2aad69093
yoonfreal <u6642005@au.ed>
2026-02-12 02:21:59 +0700

    commit1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/StudyBuddy_deployment/",
+  base: "/StudyBuddy_deploy/",
   plugins: [react()],
 })
